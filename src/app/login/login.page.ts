3f90fc6c1c6faf701f65b028973b8f7fd06ced7b
Anibal_Kaune <127349390+anibal-kaune@users.noreply.github.com>
2023-08-21 21:17:06 -0400

    registro.ts y login.ts agregados
    Falta navigatorExtras

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, NavigationExtras } from '@angular/router';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController, NavController} from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  formularioLogin: FormGroup;
+
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController,
+    public navCtrl: NavController) {
+      this.formularioLogin = this.fb.group({
+        'nombre': new FormControl("",Validators.required),
+        'password': new FormControl("",Validators.required),
+      })
+    }
 
   ngOnInit() {
+  }
+
+  async ingresar(){
+    var f = this.formularioLogin.value;
+
+    var usuario = JSON.parse(localStorage.getItem('usuario')|| '{}');
+
+    if(usuario.nombre == f.nombre && usuario.password == f.password){
+      console.log('ingresado');
+      localStorage.setItem('ingresado','true'); //bandera que indica sesion activa
+      this.navCtrl.navigateRoot('inicio');
+    }else{
+      const alert = await this.alertController.create({
+        header: '¡Ha ocurrido un error!',
+        message: 'Los datos ingresados son incorrectos.',
+        buttons: ['Aceptar']
+      });
+
+      await alert.present();
+
+    }
+
+
   }
 
 }
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +8,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  formularioRegistro: FormGroup;
 
-  constructor() { }
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController,
+    public navCtrl: NavController) { 
+      this.formularioRegistro = this.fb.group({
+      'nombre': new FormControl("", Validators.required),
+      'password': new FormControl("", Validators.required),
+      'confirmacionPassword': new FormControl("", Validators.required)
+      }); 
+  }
 
   ngOnInit() {
+  }
+
+  async guardar(){
+    var f = this.formularioRegistro.value;
+
+    if(this.formularioRegistro.invalid){
+      const alert = await this.alertController.create({
+        header: 'Campos incompletos',
+        message: 'Debes llenar todos los campos.',
+        buttons: ['Aceptar']
+      });
+
+      await alert.present();
+      return;
+    }
+    var usuario = {
+      nombre: f.nombre,
+      password: f.password
+    }
+
+    localStorage.setItem('usuario',JSON.stringify(usuario));
   }
 
 }
